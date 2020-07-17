e140aa2ee8005b87bc9d75bd8de06ea2e908a09d
jxZhang <790847400@qq.com>
2020-07-17 15:17:53 +0800

    替换端口号

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,9 +1,10 @@
 'use strict';
+console.log(process.env.PORT, process.env.$PORT);
 
 module.exports = {
   cluster: {
     listen: {
-      port: 7000,
+      port: process.env.PORT || process.env.$PORT || 7000,
       // hostname: 'https://zhangli-website.herokuapp.com',
       hostname: '127.0.0.1',
       // path: '/var/run/egg.sock',
@@ -26,7 +27,7 @@ module.exports = {
   redis: {
     client: {
       host: 'ec2-34-206-142-196.compute-1.amazonaws.com',
-      port: '31059',
+      port: 31059,
       password: 'pabcdaa67b0599d374803b7fda7b43af06dacb46e37f46faab4d6eca0deeab29a',
       db: '0',
     },
